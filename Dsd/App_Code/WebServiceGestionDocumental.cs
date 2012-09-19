0c013f23df95ca77f3ae3f567cfda2929cfad0de
unknown <alumnos@.upc.edu.pe>
2012-09-18 21:07:52 -0500

    .

---
+++ Dsd/App_Code/WebServiceGestionDocumental.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Descripción breve de WebService
+/// </summary>
+[WebService(Namespace = "http://localhost/gestiondocumental/GestionDocumentalService/", Name = "GestionDocumentalService", Description="Web Service de Gestion Documental")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// Para permitir que se llame a este servicio Web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+// [System.Web.Script.Services.ScriptService]
+public class WebService : System.Web.Services.WebService {
+
+    public WebService () {
+
+        //Eliminar la marca de comentario de la línea siguiente si utiliza los componentes diseñados 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string GetDatosGenerales(string a, string b, string c, string d) {
+
+        /**
+         
+         
+         **/
+        return "Conexión Exitosa";
+    }
+    
+}
